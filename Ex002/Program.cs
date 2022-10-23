263c2e84d28dff1c9f2ad24757eb7c0310d65496
Olga_A <zagaes2@rambler.ru>
2022-10-24 01:37:02 +0300

    example ex2 added

---
+++ Ex002/Program.cs
@@ -0,0 +1,14 @@
+﻿// Показать числа Фибоначчи
+
+int Fibonachi (int number)
+{
+    if (number==1 || number==2) return 1;
+    else
+        return Fibonachi(number-1)+Fibonachi(number-2);
+}
+
+Console.Write("Введите предельное число для вывода ряда Фибоначи: ");
+int n = int.Parse(Console.ReadLine() ?? "0");
+
+for (int i=1; i<=n; i++)
+    Console.WriteLine($"f({i}): {Fibonachi(i)}");
